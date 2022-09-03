7493551a6eea07ec0224a0bb86322b7f486871f7
jaafermnassri <mnassrijaafer0@gmail.com>
2022-09-03 23:35:20 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
+import Counter from "./component/counter"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  class App extends Component {
+    state = {show:false, counterApp:0}
+    componentDidMount() {
+      setInterval(()=> {
+        this.setState({counterApp:this.state.counterApp+1})
+      },1000)
+    }
+  }
+ 
+  render() ;{
+
+    return (
+      <div >
+        <h1>{this.state.counterApp}</h1>
+        <button onClick={()=>this.setState({show:!this.state.show})}>{this.state.show ? "hide":"show"}</button>
+        {this.state.show ? <Counter/> : null}
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
